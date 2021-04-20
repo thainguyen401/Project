caf47cc1538db3027304edf0dd27e724c68205c0
DongDucLan <56388026+DongDucLan@users.noreply.github.com>
2021-04-20 15:12:01 +0700

    Add files via upload

---
+++ Code Admin/routes.php
@@ -0,0 +1,23 @@
+<?php
+$controllers = array(
+  'introduces'=>["index"],
+  'pages' => ['home', 'error'],
+  'users' => ['', '', '', '', '', '',''] 
+  //chưa có action logout ở router
+); // Các controllers trong hệ thống và các action có thể gọi ra từ controller đó.
+// Nếu các tham số nhận được từ URL không hợp lệ (không thuộc list controller và action có thể gọi
+// thì trang báo lỗi sẽ được gọi ra.
+
+if (!array_key_exists($controller, $controllers) || 
+    !in_array($action, $controllers[$controller])) {
+  $controller = 'pages';
+  $action = 'error';
+}
+// Nhúng file định nghĩa controller vào để có thể dùng được class định nghĩa trong file đó
+//var_dump($controller);
+require_once('controllers/' . $controller . '_controller.php');
+// Tạo ra tên controller class từ các giá trị lấy được từ URL sau đó gọi ra để hiển thị trả về cho người dùng.
+$ctrclass = str_replace('_', '', ucwords($controller, '_')). 'Controller';
+$controller = new $ctrclass;
+$controller->$action(); 
+?>
